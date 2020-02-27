cd6c5d2a1d32fb27b13fafae23d509771adcc283
“Krista <stadler.krista@gmail..com>
2020-02-27 16:04:28 -0700

    Passed existence test and successfully completed file set up

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,9 +1,11 @@
-require 'test_helper'
+require './test/test_helper'
 require 'date'
 require './lib/enigma'
 
-EnigmaTest < Minitest::Test
+class EnigmaTest < Minitest::Test
 
-def test_it_exists
-  enigma = EnigmaTest.new
+  def test_it_exists
+    enigma = Enigma.new
+  end
+
 end
