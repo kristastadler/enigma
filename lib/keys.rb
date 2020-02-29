c76a7e86b6e7fbd5762e1281c4d052be5f4fc64e
“Krista <stadler.krista@gmail..com>
2020-02-29 14:02:36 -0700

    Wrote create individual keys test and passed it

--- lib/keys.rb
+++ lib/keys.rb
@@ -2,9 +2,34 @@ class Keys
   attr_reader :number
 
   def initialize(number)
-    @number = number
+    @number = number.each_char.map(&:to_i)
   end
 
+  def create_a_key
+    a_key = []
+    a_key << number[0]
+    a_key << number[1]
+    a_key = a_key.join.to_i
+  end
 
+  def create_b_key
+    b_key = []
+    b_key << number[1]
+    b_key << number[2]
+    b_key = b_key.join.to_i
+  end
 
+  def create_c_key
+    c_key = []
+    c_key << number[2]
+    c_key << number[3]
+    c_key = c_key.join.to_i
+  end
+
+  def create_d_key
+    d_key = []
+    d_key << number[3]
+    d_key << number[4]
+    d_key = d_key.join.to_i
+  end
 end
--- test/keys_test.rb
+++ test/keys_test.rb
@@ -14,7 +14,16 @@ class KeysTest < Minitest::Test
   def test_it_has_attributes
     keys = Keys.new("12345")
 
-    assert_equal "12345", keys.number
+    assert_equal [1, 2, 3, 4, 5], keys.number
+  end
+
+  def test_it_can_create_individual_keys
+    keys = Keys.new("12345")
+
+    assert_equal 12, keys.create_a_key
+    assert_equal 23, keys.create_b_key
+    assert_equal 34, keys.create_c_key
+    assert_equal 45, keys.create_d_key
   end
 
 end
