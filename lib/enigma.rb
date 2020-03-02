a97f545863c7398b38d5e385cfbb6d7f110ea76f
“Krista <stadler.krista@gmail..com>
2020-03-02 15:36:53 -0700

    decrypt letter integrated

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -109,65 +109,20 @@ class Enigma
   end
 
   def decrypt_message(message, key, date)
-    alphabet = Alphabet.new
     i = 0
 
     create_message_array(message).each do |letter|
       i += 1
       if i == 1 || i == 5 || i == 9
-        if final_shift(key, date)[:a_shift] == 27
-          encrypted_message << letter
-        else
-          shift_value = alphabet.letter_value(letter) - final_shift(key, date)[:a_shift]
-          if shift_value < 0
-            letter = alphabet.find_key(refactor_shift_value(shift_value))
-            encrypted_message << letter
-          else
-            letter = alphabet.find_key(shift_value)
-            @decrypted_message << letter
-          end
-        end
+        decrypt_letter(key, date, letter, :a_shift)
       elsif i == 2 || i == 6 || i == 10
-        if final_shift(key, date)[:b_shift] == 27
-          @decrypted_message << letter
-        else
-          shift_value = alphabet.letter_value(letter) - final_shift(key, date)[:b_shift]
-          if shift_value < 0
-            letter = alphabet.find_key(refactor_shift_value(shift_value))
-            @decrypted_message << letter
-          else
-            letter = alphabet.find_key(shift_value)
-            @decrypted_message << letter
-          end
-        end
+        decrypt_letter(key, date, letter, :b_shift)
       elsif i == 3 || i == 7 || i == 11
-        if final_shift(key, date)[:c_shift] == 27
-          encrypted_message << letter
-        else
-          shift_value = alphabet.letter_value(letter) - final_shift(key, date)[:c_shift]
-            if shift_value < 0
-            letter = alphabet.find_key(refactor_shift_value(shift_value))
-            @decrypted_message << letter
-          else
-            letter = alphabet.find_key(shift_value)
-            @decrypted_message << letter
-          end
-        end
+        decrypt_letter(key, date, letter, :c_shift)
       elsif i == 4 || i == 8 || i == 12
-        if final_shift(key, date)[:d_shift] == 27
-          @decrypted_message << letter
-        else
-          shift_value = alphabet.letter_value(letter) - final_shift(key, date)[:d_shift]
-          if shift_value < 0
-            letter = alphabet.find_key(refactor_shift_value(shift_value))
-            @decrypted_message << letter
-          else
-            letter = alphabet.find_key(shift_value)
-            @decrypted_message << letter
-          end
-        end
+        decrypt_letter(key, date, letter, :d_shift)
       end
-    end
+    end   
     @decrypted_message.join
   end
 
